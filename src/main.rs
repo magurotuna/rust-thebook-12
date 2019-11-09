4ab11619248eb419bb29c4659d3af2ba6c4610f8
Yusuke Tanaka <wing0920@gmail.com>
2019-11-09 19:04:00 +0900

    完成

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,12 @@ fn main() {
     let args = env::args().collect::<Vec<String>>();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = rust_thebook_12::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
